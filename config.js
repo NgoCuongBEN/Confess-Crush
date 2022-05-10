a6044b0e422247680a4901ea839c87830379de41
NgoCuongBEN <105308576+NgoCuongBEN@users.noreply.github.com>
2022-05-10 23:53:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Lời tỏ tình của người mặt lạ ẩn danh ",
+    introTitle: 'Phương Nga ',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
