b40e8f3b90cc426c55b4a04f434f10fac9c82131
NgoCuongBEN <105308576+NgoCuongBEN@users.noreply.github.com>
2022-06-24 20:42:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Gửi Baby dễ thương  ",
-    introTitle: 'Diễm My ',
+    titleWeb: "Gửi đến nóc nhà",
+    introTitle: 'Vũ Hà',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Lạc vào tim anh lẻ loi va vào cuộc sống bất trợt
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy ánh mắt hí đó  ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
+    btnYes: 'Thích lắm nha<33',
+    btnNo: 'Không thích à nha  :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình ( trả hiểu luôn)<3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu chịu không chịu cũng bắt buộc phải chịu <33333333',
+    btnReply: 'Gửi cho bạn nhiều<3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu chịu không chịu cũng bắt buộc phải chịu bạn hết lựa chọn <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 8h, mình qua đón đi chơi nha.',
+    messDesc: 'Hẹn bạn vào ngày 5/7 gần nhất .',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    
 }
