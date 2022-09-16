2a8dee382a42d50254add2025e85e9b1ac692bce
NgoCuongBEN <105308576+NgoCuongBEN@users.noreply.github.com>
2022-09-16 19:33:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
     titleWeb: "Gửi đến nóc nhà",
-    introTitle: 'Vũ Hà',
+    introTitle: 'Vũ Thị Thanh Đào',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi va vào cuộc sống bất trợt
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy ánh mắt hí đó  ',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên đúng hông🥰',
+    desc: 'Phải chăng em đã say ngay từ lúc anh với em cái nhau đúng hông  ',
     btnYes: 'Thích lắm nha<33',
     btnNo: 'Không thích à nha  :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình ( trả hiểu luôn)<3',
     btnReply: 'Gửi cho bạn nhiều<3',
     reply: 'Yêu thì yêu mà không yêu thì yêu chịu không chịu cũng bắt buộc phải chịu bạn hết lựa chọn <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Hẹn bạn vào ngày 5/7 gần nhất rồi nóc nhà đón về rồi chở đi chơi á nhớ đó"nấu gì đó ăn càng tốt"  .',
+    messDesc: 'Hẹn bạn vào ngày 17/09 gần nhất đáp xuống là điện nhớ nha :3 "  .',
     btnAccept: 'Okiiiii lun <3',
     
 }
